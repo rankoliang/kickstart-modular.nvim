b6b4a2f2a4eb57503b9f88e618f31bc2a6431c47
Ranko Liang <ranko@keepertax.com>
2025-01-06 13:21:21 -0500

    Add copilot

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -103,6 +103,9 @@ return {
         },
         sources = {
           {
+            name = 'copilot',
+          },
+          {
             name = 'lazydev',
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
             group_index = 0,
