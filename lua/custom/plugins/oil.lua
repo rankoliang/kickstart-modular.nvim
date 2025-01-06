facd15645efcb5fb2981aad634e5e91ea94ea0e9
Ranko Liang <ranko@keepertax.com>
2025-01-06 18:15:20 -0500

    Add init function

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -6,6 +6,9 @@ return {
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  init = function()
+    require('oil').setup()
+  end,
   keys = {
     { '-', '<CMD>Oil<CR>', desc = 'Oil.nvim' },
   },
