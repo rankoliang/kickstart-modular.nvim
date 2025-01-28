427d75982801cd7dcfefcaf3531e5c1043a4a026
Ranko Liang <ranko@keepertax.com>
2025-01-28 12:46:36 -0500

    Add gitbrowse

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -9,11 +9,25 @@ return {
       -- refer to the configuration section below
       bigfile = { enabled = true },
       dashboard = { enabled = true },
+      gitbrowse = { enabled = true },
       indent = { enabled = true, animate = { enabled = false } },
       input = { enabled = true },
       notifier = { enabled = true },
       quickfile = { enabled = true },
       statuscolumn = { enabled = true },
     },
+    keys = function()
+      local Snacks = require 'snacks'
+      return {
+        {
+          '<leader>gb',
+          function(args)
+            Snacks.gitbrowse(args)
+          end,
+          desc = 'Snacks: [G]it [B]rowse',
+          mode = { 'n', 'v' },
+        },
+      }
+    end,
   },
 }
