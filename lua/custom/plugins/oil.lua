7db98a6ee39662a69c00612f0e09cee713812381
Ranko Liang <ranko@keepertax.com>
2025-01-28 14:30:51 -0500

    Show hidden in oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,13 +2,14 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-  init = function()
-    require('oil').setup()
-  end,
   keys = {
     { '-', '<CMD>Oil<CR>', desc = 'Oil.nvim' },
   },
