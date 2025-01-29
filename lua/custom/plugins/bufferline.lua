b99d22e43c2e7b2f8fe2e1ad7aa7dd1eac877952
Ranko Liang <ranko@keepertax.com>
2025-01-28 20:30:52 -0500

    Add bufferline highlights

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -2,7 +2,11 @@ return {
   {
     'akinsho/bufferline.nvim',
     version = '*',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin/nvim' },
+    opts = function(_, opts)
+      if (vim.g.colors_name or ''):find 'catppuccin' then
+        opts.highlights = require('catppuccin.groups.integrations.bufferline').get()
+      end
+    end,
   },
 }
