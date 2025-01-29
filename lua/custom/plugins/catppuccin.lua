2434255e5eb012a81baa32de5dfbcc797f0631a2
Ranko Liang <ranko@keepertax.com>
2025-01-28 20:41:43 -0500

    Add catppuccin integrations

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -10,6 +10,12 @@ return {
     end,
     integrations = {
       fzf = true,
+      snacks = true,
+      lsp_trouble = true,
+      neotest = true,
+      dadbod_ui = true,
+      which_key = true,
+      diffview = true,
     },
   },
   init = function()
