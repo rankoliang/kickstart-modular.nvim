7b3249d75f63e05c83f6f8d83a707a1176e373b3
Ranko Liang <rankoliang@gmail.com>
2025-01-31 14:23:08 -0500

    Add json formatter

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -41,6 +41,7 @@ return {
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
         javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
