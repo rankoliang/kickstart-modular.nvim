4af7c98d561d730f4c02e9dda5221f79c7383a94
Ranko Liang <rankoliang@gmail.com>
2025-09-22 15:13:40 -0400

    Update packages

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -5,7 +5,7 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin/nvim' },
     opts = function(_, opts)
       if (vim.g.colors_name or ''):find 'catppuccin' then
-        opts.highlights = require('catppuccin.groups.integrations.bufferline').get()
+        opts.highlights = require('catppuccin.groups.integrations.bufferline').get_theme()
       end
     end,
   },
